6580ca253f589e90d7c3e000932ed663562bc3f3
Enderson Siqueira <endsiqueira@gmail.com>
2024-02-08 23:14:40 -0300

    rename main class

---
+++ src/main/java/com/learning/sessions/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.sessions.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSessionsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSessionsAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/sessions/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.sessions.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSessionsAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
